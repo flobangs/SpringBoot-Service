24c809bfb0f3c9b2f02bb57d461f8683dae93446
flobangs <flobangs.korea@gmail.com>
2023-05-16 01:17:28 +0900

    AccountController 완성

--- src/main/java/shop/mtcoding/servicebank/dto/account/AccountResponse.java
+++ src/main/java/shop/mtcoding/servicebank/dto/account/AccountResponse.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 import shop.mtcoding.servicebank.core.util.MyDateUtils;
 import shop.mtcoding.servicebank.model.account.Account;
+import shop.mtcoding.servicebank.model.transaction.Transaction;
 import shop.mtcoding.servicebank.model.user.User;
 
 import java.util.ArrayList;
@@ -75,5 +76,41 @@ public class AccountResponse {
                 this.balance = account.getBalance();
             }
         }
+    }
+
+    @Getter @Setter
+    public static class TransferOutDTO {
+        private Long id;
+        private Integer number;
+        private Long balance;
+        private TransactionDTO transaction;
+        private Account withdrawAccount;
+
+        public TransferOutDTO(Account account, Transaction transaction) {
+            this.id = account.getId();
+            this.number = account.getNumber();
+            this.balance = account.getBalance();
+            this
+        }
+
+        @Getter @Setter
+        public class TransactionDTO {
+            private Long id;
+            private String sender;
+            private String receiver;
+            private Long amount;
+            private Long balance;
+            private String createdAt;
+
+            public TransactionDTO(Transaction transaction) {
+                this.id = transaction.getId();
+                this.sender = transaction.getWithdrawAccount().getNumber()+"";
+                this.receiver = transaction.getDepositAccount().getNumber()+"";
+                this.amount = transaction.getAmount();
+                this.balance = transaction.getWithdrawAccountBalance();
+                this.createdAt = MyDateUtils.toStringFormat(transaction.getCreatedAt());
+            }
+        }
+
     }
 }
--- src/main/java/shop/mtcoding/servicebank/service/AccountService.java
+++ src/main/java/shop/mtcoding/servicebank/service/AccountService.java
@@ -3,13 +3,18 @@ package shop.mtcoding.servicebank.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.expression.ExpressionException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import shop.mtcoding.servicebank.core.exception.Exception400;
+import shop.mtcoding.servicebank.core.exception.Exception401;
 import shop.mtcoding.servicebank.core.exception.Exception404;
 import shop.mtcoding.servicebank.dto.ResponseDTO;
 import shop.mtcoding.servicebank.dto.account.AccountRequest;
 import shop.mtcoding.servicebank.dto.account.AccountResponse;
+import shop.mtcoding.servicebank.dto.transaction.TransactionResponse;
 import shop.mtcoding.servicebank.model.account.Account;
 import shop.mtcoding.servicebank.model.account.AccountRepository;
+import shop.mtcoding.servicebank.model.transaction.Transaction;
+import shop.mtcoding.servicebank.model.transaction.TransactionRepository;
 import shop.mtcoding.servicebank.model.user.User;
 import shop.mtcoding.servicebank.model.user.UserRepository;
 
@@ -21,6 +26,7 @@ import java.util.Optional;
 public class AccountService {
     private final AccountRepository accountRepository;
     private final UserRepository userRepository;
+    private final TransactionRepository transactionRepository;
 
     public AccountResponse.SaveOutDTO 계좌등록(AccountRequest.SaveInDTO saveInDTO, Long id) {
         User userPS = userRepository.findById(id).orElseThrow(
@@ -51,9 +57,48 @@ public class AccountService {
 
         User userPS = userRepository.findById(userId).orElseThrow(
                 () -> new Exception404("유저를 찾을 수 없습니다"));
-
         List<Account> accountListPS = accountRepository.findByUserId(userId);
 
         return new AccountResponse.ListOutDTO(userPS, accountListPS);
+    }
+
+    @Transactional
+    public AccountResponse.TransferOutDTO 계좌이체(AccountRequest.TransferInDTO transferInDTO, Long userId) {
+        if(transferInDTO.getWithdrawPassword() == transferInDTO.getDepositNumber()) {
+            throw new Exception400("withdrawAccountNumber", "입출금계좌가 동일할 수 없습니다");
+        }
+
+        Account withdrawAccountPS = accountRepository.findByNumber(transferInDTO.getWithdrawNumber())
+                .orElseThrow(
+                        () -> new Exception404("출금계좌를 찾을 수 없습니다"));
+        Account depositAccountPS = accountRepository.findByNumber(transferInDTO.getDepositNumber())
+                .orElseThrow(
+                        () -> new Exception404("입금계좌를 찾을 수 없습니다"));
+
+        // 출금계좌 인증(로그인한 사람과 동일한지 체크)
+        withdrawAccountPS.checkOwner(userId);
+
+        // 출금계좌 비밀번호 확인
+        withdrawAccountPS.checkSamePassword(transferInDTO.getWithdrawPassword());
+
+        // 출금계좌 잔액 확인
+        withdrawAccountPS.checkBalance(transferInDTO.getAmount());
+
+        // 이체하기
+        withdrawAccountPS.withdraw(transferInDTO.getAmount());
+        withdrawAccountPS.deposit(transferInDTO.getAmount());
+
+        // 거래 내역 남기기
+        Transaction transaction = Transaction.builder()
+                .withdrawAccount(withdrawAccountPS)
+                .depositAccount(depositAccountPS)
+                .withdrawAccountBalance(withdrawAccountPS.getBalance())
+                .depositAccountBalance(depositAccountPS.getBalance())
+                .amount(transferInDTO.getAmount())
+                .build();
+
+        Transaction transactionPS = transactionRepository.save(transaction);
+
+        return new AccountResponse.TransferOutDTO(withdrawAccountPS, transactionPS);
     }
 }
